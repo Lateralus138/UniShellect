fada294f7ad0a5acacf90c2c0bd15845738c08a7
Lateraus138 <faithnomoread@yahoo.com>
2020-08-24 13:47:30 -0500

    Possibly fixed AppIMage...

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 chmod +x ./appimagetool-x86_64.AppImage
-./appimagetool-x86_64.AppImage appimage/unishellect.AppDir
+./appimagetool-x86_64.AppImage appimage/unishellect.AppDir dist/appimage/unishellect-x86_64.AppImage
